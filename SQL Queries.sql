aa269b58cc01af88415fb0b8a9cb1dc91ffe1411
SheikhAnsHaider <122617422+SheikhAnsHaider@users.noreply.github.com>
2023-01-19 17:05:16 +0500

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -18,9 +18,9 @@ WHERE grommet_product_keywords.keyword='Hair accessor' AND grommet_products.is_s
 Query 3
 ---------------------------------------------
 SELECT grommet_products.product_name, grommet_products.product_img_url, grommet_products.product_url, grommet_products.product_price_min, grommet_products.product_short_description
-FROM ((((grommet_products
-JOIN grommet_product_categories ON grommet_products.id = grommet_product_categories.product_id)
-JOIN grommet_gifts_categories ON grommet_product_categories.product_category_id = grommet_gifts_categories.id) 
-JOIN grommet_product_to_keyword ON grommet_products.id = grommet_product_to_keyword.product_id) 
-JOIN grommet_product_keywords ON grommet_product_to_keyword.keyword_id = grommet_product_keywords.id)
-WHERE grommet_gifts_categories.sub_category='Beauty & Personal Care' OR grommet_product_keywords.keyword='Aromatherapy' AND grommet_products.is_sold_out = false;
+FROM  grommet_gifts_categories 
+INNER JOIN grommet_product_categories ON grommet_gifts_categories.id = grommet_product_categories.product_category_id 
+INNER JOIN grommet_product_keywords ON grommet_gifts_categories.id = grommet_product_keywords.id 
+INNER JOIN grommet_product_to_keyword ON grommet_product_keywords.id = grommet_product_to_keyword.keyword_id 
+INNER JOIN grommet_products ON grommet_product_categories.product_id = grommet_products.id AND grommet_product_to_keyword.product_id = grommet_products.id
+WHERE grommet_gifts_categories.sub_category='Beauty & Personal Care' AND grommet_products.is_sold_out = false OR grommet_product_keywords.keyword='Aromatherapy' ;
